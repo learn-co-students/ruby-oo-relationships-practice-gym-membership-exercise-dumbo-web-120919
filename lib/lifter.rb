eb9bc4c9a3d7d930fd32b3ec63812245c85294b5
itslizkim <elizabeth.kim93@gmail.com>
2019-12-16 17:46:07 -0500

    deliverables done

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,61 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.lifter == self}
+  end
+
+  # Get a list of all the gyms that a specific lifter has memberships to
+  def gyms
+    self.memberships.map do |gym_instance| 
+      gym_instance.gym
+    end
+  end
+
+  # Get the average lift total of all lifters
+  # the total number of lifters
+  # total of lift totals
+  #average is numLifters over totalLiftTotals
+  
+  def self.average_lift
+    self.all.sum(&:lift_total)/self.all.length
+  end
+
+  def total_cost
+    self.memberships.sum(&:cost)
+  end
+
+  def new_membership(gym,cost)
+    Membership.new(self, gym, cost)
   end
 end
+
+
+
+# Get a list of all lifters
+
+# Get a list of all the memberships that a specific lifter has
+
+# Get a list of all the gyms that a specific lifter has memberships to
+
+# Get the average lift total of all lifters
+
+# Get the total cost of a specific lifter's gym memberships
+
+# Given a gym and a membership cost, sign a specific lifter up for a new gym
+
+
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,21 @@
 # You don't need to require any of the files in lib or pry.
 # We've done it for you here.
 require_relative '../config/environment.rb'
+#Lifter.new (name, lift_total)
+l1 = Lifter.new("Holly", 950)
+l2 = Lifter.new("Fred", 1300)
+l3 = Lifter.new("Sally", 1200)
+#Gym.new(name)
+g1 = Gym.new ("Lifting Spirits")
+g2 = Gym.new ("Death Box")
+g3 = Gym.new ("Ultra Gains")
+#Membership.new(lifter, gym ,cost)
+m1 = Membership.new(l1, g3, 120)
+m2 = Membership.new(l1, g1, 90)
+m3 = Membership.new(l2, g2, 100)
+m4 = Membership.new(l2, g1, 90)
+m5 = Membership.new(l3, g2, 100)
+m6 = Membership.new(l3, g3, 120)
 
 # test code goes here
 
