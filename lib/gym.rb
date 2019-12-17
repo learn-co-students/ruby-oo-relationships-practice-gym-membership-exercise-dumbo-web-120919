89e8d9b9fb91caec096809f45d0d7f120951a172
teapuddles <kingsley.gleeson@gmail.com>
2019-12-17 16:18:41 -0500

    Done

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,43 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |membership_inst|
+      membership_inst.gym == self
+    end
+  end
+  
+  def lifters
+    memberships.map do |membership_inst|
+      membership_inst.lifter
+    end
+  end
+
+  def lifter_names
+    lifters.map do |lifter_inst|
+      lifter_inst.name
+    end
+  end
+
+  def lift_totals
+    lifters.reduce(0) do |sum, lifter|
+      sum + lifter.lift_total
+    end
+  end
+
 end
+
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,18 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+g1 = Gym.new("Global Gym")
+g2 = Gym.new("Average Joes")
+g3 = Gym.new("Planet Fitness")
+
+l1 = Lifter.new("Lunk", 200)
+l2 = Lifter.new("Skinny Mini", 50)
+l3 = Lifter.new("Average Joe", 110)
+
+m1 = Membership.new(200, g1, l1)
+m2 = Membership.new(60, g3, l3)
+m3 = Membership.new(10, g2, l2)
+m4 = Membership.new(10, g2, l1)
 
 binding.pry
 
